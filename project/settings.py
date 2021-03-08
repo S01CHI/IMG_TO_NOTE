ff787f2ccc8084e8a7c099d802ed1cbbf283a6a0
S01chI <suzukisuzup@gmail.com>
2021-03-08 18:44:07 +0900

    settings change

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=japbnl&5^p%b$)(d!2%n@g9zwj^6mp3&o&d+u#37j2!))ebv0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
